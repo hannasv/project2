8b5c7053be728ea253da1e4eb3f49ba89bd79444
Hanna Svennevik <svennevikh@gmail.com>
2018-10-23 12:21:33 +0200

    updatet activation func with ELU

--- Costfunctions.py
+++ Costfunctions.py
@@ -8,9 +8,11 @@ class Costfunctions:
         self.lmd = lmd,
         self.p = None
 
+    # Computes the residuals = error
     def r(self, y): # resuduals.
         return y-self.p
 
+    # Currently not used anywhere.
     def update_weights(self, new):
         self.w = new
 
@@ -18,8 +20,13 @@ class Costfunctions:
         if (key == "sigmoid"):
             self.p = 1. / (1. + np.exp(-np.clip(Xw, -250, 250)))
             return self.p
-        elif(key == "elu"):
-            return 0
+        elif(key = "ELU"):
+            if (z >= 0):
+                return z
+            else:
+                return alpha*(np.exp(z)-1)
+        else:
+            print("Unvalide keyword argument. Use siogmoid or ELU for activation.")
 
 class Cost_OLS(Costfunctions):
 
@@ -29,7 +36,7 @@ class Cost_OLS(Costfunctions):
         self.lmd = lmd,
 
     def calculate(self, X, y,  w, key = "sigmoid"):
-        self.p = self.activation(X, key)
+        #self.p = self.activation(X, key)
         return -y.dot(np.log(self.p)) - ((1 - y).dot(np.log(1 - self.p)))
 
     def grad(self, X, w, errors):
@@ -44,7 +51,8 @@ class Cost_Ridge(Costfunctions):
         self.lmd = lmd,
 
     def calculate(self, X, y, w, key = "sigmoid"):
-        self.p = self.activation(X, key)
+        #net_input = np.dot(X, self.w_[1:]) + self.w_[0]
+        #self.p = self.activation(X, key)
         return -y.dot(np.log(self.p)) - ((1 - y).dot(np.log(1 - self.p))) + self.lmd*w[1:] + w[0]
 
     def grad(self, X, w,errors):
@@ -58,7 +66,7 @@ class Cost_Lasso(Costfunctions):
         self.lmd = lmd,
 
     def calculate(self, X, y, w, key = "sigmoid"):
-        self.p = self.activation(X, "sigmoid")
+        #self.p = self.activation(X, "sigmoid")
         return -y.dot(np.log(self.p)) - ((1 - y).dot(np.log(1 - self.p))) + self.lmd
 
     def grad(self, X,w, errors):
